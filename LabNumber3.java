d4801de7339a153a67718fecff8d907e7895acb2
Acastalope <43508040+Acastalope@users.noreply.github.com>
2018-10-11 12:42:02 -0400

    Add files via upload

---
+++ LabNumber3.java
@@ -0,0 +1,49 @@
+package LabNumber3;
+
+import java.util.Scanner;
+
+public class LabNumber3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+				
+		System.out.println("Enter an integer between 1 and 100");
+		x = sc.nextInt();		
+		
+				
+		if (x % 2 == 0)  
+			if (x > 60)
+				if (x < 100)
+		
+		System.out.println(x + " " + "Even");
+		
+		if (x % 2 == 0)  
+			if(x >= 2)
+				if(x <= 25)
+		
+		System.out.println("Even and less than 25." );
+			
+		if (x % 2 == 0)  
+			if(x >= 26)
+				if(x <= 60)
+					
+		System.out.println("Even");
+		
+		if (x % 2 !=0)  
+			if (x < 60)
+				if (x > 0)
+					
+		System.out.println(x + " " + "Odd");
+		
+		if (x % 2 !=0)  
+			if (x > 60)
+				if (x < 100)
+					
+		System.out.println(x + " " + "Odd and over 60");
+		
+			}
+		}
+		
